1439e710b357b48189df75c77bfe8c0568a4a0a2
quochoi227 <quochoilam2207@gmail.com>
2025-12-10 22:33:02 +0700

    Fix bug

--- frontend/src/pages/ChatAppPage.tsx
+++ frontend/src/pages/ChatAppPage.tsx
@@ -1,4 +1,4 @@
-import ChatWindowLayout from '@/components/chat/ChatWindowLayout'
+// import ChatWindowLayout from '@/components/chat/ChatWindowLayout'
 import { AppSidebar } from '@/components/sidebar/app-sidebar'
 import { SidebarProvider } from '@/components/ui/sidebar'
 
@@ -6,9 +6,7 @@ function ChatAppPage() {
   return (
     <SidebarProvider>
       <AppSidebar />
-      <div className="flex h-screen w-full p-2">
-        <ChatWindowLayout />
-      </div>
+      <div className="flex h-screen w-full p-2">{/* <ChatWindowLayout /> */}</div>
     </SidebarProvider>
   )
 }
