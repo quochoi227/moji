691994d0d2c02f38854c066447c039b17a8ea80f
quochoi227 <quochoilam2207@gmail.com>
2025-12-11 11:50:43 +0700

    Add heading

--- frontend/src/components/chat/ChatWellcomeScreen.tsx
+++ frontend/src/components/chat/ChatWellcomeScreen.tsx
@@ -1,10 +1,10 @@
 import { SidebarInset } from '../ui/sidebar'
-// import ChatWindowHeader from './ChatWindowHeader'
+import ChatWindowHeader from './ChatWindowHeader'
 
 function ChatWellcomeScreen() {
   return (
     <SidebarInset className="flex w-full h-full bg-transparent">
-      {/* <ChatWindowHeader chat={{}} /> */}
+      <ChatWindowHeader />
       <div className="flex bg-primary-foreground rounded-2xl flex-1 items-center justify-center">
         <div className="text-center">
           <div className="size-24 mx-auto mb-6 bg-gradient-chat rounded-full flex items-center justify-center shadow-glow pulse-ring">
